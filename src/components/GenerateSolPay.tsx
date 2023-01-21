4548add2943ec2d7dd1526abbf2f58ddf104c335
Owen <owenventer99@gmail.com>
2023-01-21 10:34:06 +0200

    working SolanaPay QR code with changable amount

--- src/components/GenerateSolPay.tsx
+++ src/components/GenerateSolPay.tsx
@@ -1,58 +1,67 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { LAMPORTS_PER_SOL, TransactionSignature,Keypair } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL, TransactionSignature, PublicKey,Keypair } from '@solana/web3.js';
 import { FC, useCallback,useState} from 'react';
 import { notify } from "../utils/notifications";
 import QRCode from "react-qr-code";
-import { Cluster, clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
-import { encodeURL, createQR } from '@solana/pay';
 import BigNumber from 'bignumber.js';
+import { encodeURL, createQR } from '@solana/pay';
 
 export const GenerateSolPay: FC = () => {
     const { connection } = useConnection();
-    const { publicKey } = useWallet();
+    //const { publicKey } = useWallet();
+    //const [amount1, setValue] = useState("0.1");
 
+    //Standard vars
+        const recipient = new PublicKey("9AihNo84zvCbJNPH6aceCa3SuGDCJZrRuJ3XR1SypZ5n");
+        //let amount = new BigNumber(0.1);
+        let reference = new Keypair().publicKey;
+        const label = 'SolMerchant Transaction';
+        const message = 'Payment to SolMerchant';
+        const memo = 'SM';
+        const [amount,setAmount]=useState(BigNumber(0.1));
+        const [url, setURL] = useState(encodeURL({ recipient, amount, reference, label, message, memo }));
+        
 
-    function setValue(num){
-        amount=new BigNumber(num)
-    }
-    //SolanaPay consts
-    console.log('2. 🛍 Simulate a customer checkout \n');
-    let recipient = new PublicKey('MERCHANT_WALLET');
-    let amount= new BigNumber(20);
-    const reference = new Keypair().publicKey;
-    const label = 'SolMerchant';
-    const message = 'Pay the ammount owed';
-    const memo = 'JC#4098';
+        // const changeURL = (inURL) => {
+        //     setURL(inURL);
+        //   };
+    function generateQR(num){
 
-    //SolanaPay url
-    console.log('3. 💰 Create a payment request link \n');
-    const url = encodeURL({ recipient, amount, reference, label, message, memo });
-
-    //SolanaPay qrCode
-    const qrCode = createQR(url);
-    const element = document.getElementById('qr-code');
-    // append QR code to the element
-    qrCode.append(element);
-
-
+        //const qrValue="solana:"+publicKey+"?amount="+amount1+"&&label=SnapSol+Payment"
+        console.log('2. 🛍 Simulate a customer checkout \n');
+        reference = new Keypair().publicKey;
+        setAmount(new BigNumber(num));
+        setURL(encodeURL({ recipient, amount, reference, label, message, memo }));
+        console.log("NEW URL: "+url);
+}
+    
     return (
-        <div>
+        <div> 
+        <div className="flex flex-col space-y-4">
+            <div>
             <label className="text-center text-1xl font-bold">Enter Amount:</label>
             <br/>
             
             <input className="text-center text-1xl md:pl-15 font-bold text-black py-2"
           type="number"
-          onChange={(e) => setValue(e.target.value)}
+          onChange={(e) => setAmount(new BigNumber(e.target.value))}
           placeholder="Amount to pay"/>
+          </div>
             <br/>
+            <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => { setURL(encodeURL({ recipient, amount, reference, label, message, memo })); }}  
+                >Generate QR  
+            </button>  
+            </div>
+            <div>
             <QRCode
                 className="py-5"
                 size={256}
                 style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                value={""+url}
+                value={url+""}
                 viewBox={`0 0 256 256`}
             />
-            
-        </div>
+            </div>
+         </div>
     );
 };
